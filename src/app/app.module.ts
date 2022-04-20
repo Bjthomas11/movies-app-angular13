88ebb3d14406a688634ce542d91cf120e5ac4a81
Brian Thomas <brianthomas571@gmail.com>
2022-04-20 14:57:37 -0500

    completed slide functionalty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { CommonModule } from '@angular/common';
     CoreModule,
     HttpClientModule,
     CommonModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,14 +1,38 @@
 import { Movie } from './../../../core/models/movie';
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'comp-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slide', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   @Input() movies: Movie[] = [];
+  currentSlideIndex: number = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.movies.length;
+    }, 5000);
+  }
 }
