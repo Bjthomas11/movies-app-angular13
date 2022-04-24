5293b539971d7e4badc707690923a7f0803205b1
Brian Thomas <brianthomas571@gmail.com>
2022-04-24 15:13:04 -0500

    video updates

--- src/app/shared/components/iframe/iframe.component.ts
+++ src/app/shared/components/iframe/iframe.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'comp-iframe',
@@ -11,8 +11,21 @@ export class IframeComponent implements OnInit {
   @Input() site: string = 'YouTube';
   @Input() key: string | null = null;
   @Input() title: string = '';
+  videoURL: SafeResourceUrl = '';
 
-  constructor(private domSanitizer: DomSanitizer) {}
+  constructor(private domSanitizer: DomSanitizer) {
+    switch (this.site) {
+      case 'YouTube':
+        this.videoURL = this.getSafeURL(
+          'https://www.youtube.com/embed/' + this.key
+        );
+        break;
+      case 'Vimeo':
+        this.videoURL = this.getSafeURL(
+          'https://player.vimeo.com/video/' + this.key
+        );
+    }
+  }
 
   getSafeURL(url: string) {
     return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
