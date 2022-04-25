7020ed5e88d4bd001311929acf17d325a8ae444b
Brian Thomas <brianthomas571@gmail.com>
2022-04-24 20:22:07 -0500

    build project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://api.themoviedb.org/3',
+  KEY: '305cf5d9dcc3ea93b3a6411ba033664b',
+  CONFIG: '/configuration',
 };
